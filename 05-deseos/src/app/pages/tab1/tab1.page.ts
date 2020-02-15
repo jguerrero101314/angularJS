c21840657593687f32ed23c344d868127846ebd3
jguerrero101314 <joelg1014@hotmail.com>
2020-02-15 11:45:14 -0500

    130. Editar elementos de otras listas

--- 05-deseos/src/app/pages/tab1/tab1.page.ts
+++ 05-deseos/src/app/pages/tab1/tab1.page.ts
@@ -1,52 +1,57 @@
-import { Component } from '@angular/core';
-import { DeseosService } from '../../services/deseos.service';
-import { Router } from '@angular/router';
-import { AlertController } from '@ionic/angular';
+import { Component } from "@angular/core";
+import { DeseosService } from "../../services/deseos.service";
+import { Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { Lista } from "src/app/models/lista.model";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
-
-  constructor( public deseosService:DeseosService,
-               private router: Router,
-               private alertController: AlertController) {}
+  constructor(
+    public deseosService: DeseosService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   async agregarLista() {
     const alert = await this.alertController.create({
-      header: 'Nueva lista',
+      header: "Nueva lista",
       inputs: [
         {
-          name: 'titulo',
-          type: 'text',
-          placeholder: 'Nombre de la lista'
+          name: "titulo",
+          type: "text",
+          placeholder: "Nombre de la lista"
         }
       ],
       buttons: [
-       {
-        text: ' Cancelar ',
-        role: ' cancel',
-        handler: () => {
-          console.log(' Cancelar ');
-        }
-       },
-       {
-         text: 'Crear',
-         handler: (data) => {
-          console.log(data);
-          if ( data.titulo.length === 0 ){
-            return;
+        {
+          text: " Cancelar ",
+          role: " cancel",
+          handler: () => {
+            console.log(" Cancelar ");
           }
-          //crear la lista
-          const listaId =  this.deseosService.crearLista(data.titulo);
-          this.router.navigateByUrl(`/tabs/tab1/agregar/${ listaId }`);
-         }
-       }
+        },
+        {
+          text: "Crear",
+          handler: data => {
+            console.log(data);
+            if (data.titulo.length === 0) {
+              return;
+            }
+            //crear la lista
+            const listaId = this.deseosService.crearLista(data.titulo);
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+          }
+        }
       ]
     });
 
     await alert.present();
+  }
+  listaSelecciona(lista: Lista) {
+    this.router.navigateByUrl(`/tabs/tab1/agregar/${lista.id}`);
   }
 }
