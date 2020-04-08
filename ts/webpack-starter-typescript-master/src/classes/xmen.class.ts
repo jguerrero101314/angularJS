20334073cda676d93c6e35fbe022e71312405b12
jguerrero101314 <joelg1014@hotmail.com>
2020-04-08 12:49:13 -0500

    25. Importaciones * URL

---
+++ ts/webpack-starter-typescript-master/src/classes/xmen.class.ts
@@ -0,0 +1,9 @@
+export class Xmen{
+    constructor(
+    public nombre:string,
+    public clave:string
+    ){}
+    imprimir(){
+        console.log(`${this.nombre} - ${this.clave}`);
+    }
+}
---
+++ ts/webpack-starter-typescript-master/src/index.ts
@@ -0,0 +1,6 @@
+import { Xmen } from "./classes/xmen.class";
+
+const wolverine = new Xmen('Logan','wolverine');
+// console.log(wolverine);
+
+wolverine.imprimir();
