1ec8a6d86ed9a17df7348efd19502c2e69dab1d3
jguerrero101314 <joelg1014@hotmail.com>
2020-05-27 08:03:10 -0500

    168. Conectar el formulario de registro con una instancia del modelo de usuario

--- 07-bonus-login/src/app/pages/registro/registro.component.ts
+++ 07-bonus-login/src/app/pages/registro/registro.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { UsuarioMdoel } from "../../models/usuario.model";
 
 @Component({
-  selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  selector: "app-registro",
+  templateUrl: "./registro.component.html",
+  styleUrls: ["./registro.component.css"],
 })
 export class RegistroComponent implements OnInit {
+  usuario: UsuarioMdoel;
 
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {
+    this.usuario = new UsuarioMdoel();
+    this.usuario.email = 'joelg1014@hotmail.com';
+  }
+  onSubmit(){
+    console.log("Formulario enviado");
+    console.log(this.usuario);
 
-  ngOnInit() { }
-
-
+  }
 }
