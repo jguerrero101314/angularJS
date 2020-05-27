b43672eb62b4a852bd18d5fe59403997862f6bfb
jguerrero101314 <joelg1014@hotmail.com>
2020-05-27 09:10:54 -0500

    171. Tarea - Pantalla de login y sus validaciones

--- 07-bonus-login/src/app/pages/login/login.component.ts
+++ 07-bonus-login/src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioMdoel } from "../../models/usuario.model";
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usuario: UsuarioMdoel;
 
   constructor() { }
 
   ngOnInit() {
+    this.usuario = new UsuarioMdoel();
+
+  }
+
+  login(form: NgForm){
+    if(form.invalid){return;}
+    console.log(this.usuario);
+    console.log("Imprimir si el formulario es valido");
+    console.log(form);
+
   }
 
 }
