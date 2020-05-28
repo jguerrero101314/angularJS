64f1ade0c04673fe5790773a9e5729ba6ce50c8b
jguerrero101314 <joelg1014@hotmail.com>
2020-05-28 12:50:03 -0500

    192. Template: Mostrar errores y mensajes

--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -17,6 +17,13 @@ export class TemplateComponent implements OnInit {
   ngOnInit(): void {}
   guardar(forma: NgForm) {
     console.log(forma);
+    if(forma.invalid){
+
+      Object.values(forma.controls).forEach(control => {
+        control.markAsTouched();
+      });
+      return;
+    }
     console.log(forma.value);
   }
 }
