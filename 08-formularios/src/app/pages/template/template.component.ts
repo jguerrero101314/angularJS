0865a0fc009c9a3fe1a0af00da640f9843fab848
jguerrero101314 <joelg1014@hotmail.com>
2020-05-28 12:11:48 -0500

    187. Template: ngModel - ngSubmit - Referencias locales a los elementos HTML

--- 08-formularios/src/app/pages/template/template.component.ts
+++ 08-formularios/src/app/pages/template/template.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-template',
-  templateUrl: './template.component.html',
-  styleUrls: ['./template.component.css']
+  selector: "app-template",
+  templateUrl: "./template.component.html",
+  styleUrls: ["./template.component.css"],
 })
 export class TemplateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  guardar() {
+    console.log("Hola");
   }
-
 }
