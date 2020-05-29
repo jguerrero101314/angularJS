1e6d58a3df37aebbfbae99735e6aaf48ce11e60a
jguerrero101314 <joelg1014@hotmail.com>
2020-05-29 09:04:47 -0500

    203. Reactivo: Reset y carga de la data inicial

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -11,48 +11,77 @@ export class ReactiveComponent implements OnInit {
 
   constructor(private fb: FormBuilder) {
     this.crearFormulario();
+    this.cargarDataAlFormulario();
   }
 
   ngOnInit(): void {}
 
-  get nombreNoValido(){
-    return this.forma.get('nombre').invalid && this.forma.get('nombre').touched;
+  get nombreNoValido() {
+    return this.forma.get("nombre").invalid && this.forma.get("nombre").touched;
   }
-  get apellidoNoValido(){
-    return this.forma.get('apellido').invalid && this.forma.get('apellido').touched;
+  get apellidoNoValido() {
+    return (
+      this.forma.get("apellido").invalid && this.forma.get("apellido").touched
+    );
   }
-  get correoNoValido(){
-    return this.forma.get('correo').invalid && this.forma.get('correo').touched;
+  get correoNoValido() {
+    return this.forma.get("correo").invalid && this.forma.get("correo").touched;
   }
-  get distritoNoValido(){
-    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  get distritoNoValido() {
+    return (
+      this.forma.get("direccion.distrito").invalid &&
+      this.forma.get("direccion.distrito").touched
+    );
   }
-  get ciudadNoValida(){
-    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  get ciudadNoValida() {
+    return (
+      this.forma.get("direccion.ciudad").invalid &&
+      this.forma.get("direccion.ciudad").touched
+    );
   }
   crearFormulario() {
     this.forma = this.fb.group({
-      nombre  : ['', [Validators.required, Validators.minLength(5)]],
-      apellido: ['', [Validators.required, Validators.minLength(5)]],
-      correo  : ['',[ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      nombre: ["", [Validators.required, Validators.minLength(5)]],
+      apellido: ["", [Validators.required, Validators.minLength(5)]],
+      correo: [
+        "",
+        [
+          Validators.required,
+          Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3}$"),
+        ],
+      ],
       direccion: this.fb.group({
-        distrito:['',Validators.required],
-        ciudad: ['', Validators.required]
-      })
+        distrito: ["", Validators.required],
+        ciudad: ["", Validators.required],
+      }),
     });
   }
-  guardar(){
+  guardar() {
     console.log(this.forma);
     if (this.forma.invalid) {
       return Object.values(this.forma.controls).forEach((control) => {
-        if(control instanceof FormGroup){
-          Object.values(control.controls).forEach(control => control.markAsTouched());
-        }else{
+        if (control instanceof FormGroup) {
+          Object.values(control.controls).forEach((control) =>
+            control.markAsTouched()
+          );
+        } else {
           control.markAsTouched();
         }
       });
-
     }
+  }
+  cargarDataAlFormulario() {
+    // this.forma.setValue({
+      this.forma.reset({
+      nombre: "Alexander",
+      apellido: "Guerrero",
+      correo: "abc@hotmail.com",
+      direccion: {
+        distrito: "Panama",
+        ciudad: "Panama",
+      },
+    });
+    // Posteo de informacion
 
   }
 }
