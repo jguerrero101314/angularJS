264b0487e3f274018c9e5d780450225957d4fd95
jguerrero101314 <joelg1014@hotmail.com>
2020-05-29 08:55:33 -0500

    202. Reactivo: Validación visual de los campos anidados

--- 08-formularios/src/app/pages/reactive/reactive.component.ts
+++ 08-formularios/src/app/pages/reactive/reactive.component.ts
@@ -24,6 +24,12 @@ export class ReactiveComponent implements OnInit {
   get correoNoValido(){
     return this.forma.get('correo').invalid && this.forma.get('correo').touched;
   }
+  get distritoNoValido(){
+    return this.forma.get('direccion.distrito').invalid && this.forma.get('direccion.distrito').touched;
+  }
+  get ciudadNoValida(){
+    return this.forma.get('direccion.ciudad').invalid && this.forma.get('direccion.ciudad').touched;
+  }
   crearFormulario() {
     this.forma = this.fb.group({
       nombre  : ['', [Validators.required, Validators.minLength(5)]],
@@ -39,7 +45,11 @@ export class ReactiveComponent implements OnInit {
     console.log(this.forma);
     if (this.forma.invalid) {
       return Object.values(this.forma.controls).forEach((control) => {
-        control.markAsTouched();
+        if(control instanceof FormGroup){
+          Object.values(control.controls).forEach(control => control.markAsTouched());
+        }else{
+          control.markAsTouched();
+        }
       });
 
     }
