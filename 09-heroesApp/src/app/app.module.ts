d40b605514ea16360f188b353b1cd2f2a708f31b
jguerrero101314 <joelg1014@hotmail.com>
2020-05-30 11:44:48 -0500

    217. Modelo para manejar Héroes y formulario de edición

--- 09-heroesApp/src/app/app.module.ts
+++ 09-heroesApp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
@@ -14,7 +15,8 @@ import { HeroesComponent } from './pages/heroes/heroes.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from '../../models/heroe.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-heroe',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe.component.css']
 })
 export class HeroeComponent implements OnInit {
+  heroe = new HeroeModel();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  guardar(form: NgForm){
+    if(form.invalid){
+      console.log("formulario no valido");
+      return;
+    }
+    console.log(form);
+      console.log(this.heroe);
+
   }
 
 }
