5b1022921c366dfc5b823180226fa057d2e13e4e
jguerrero101314 <joelg1014@hotmail.com>
2020-05-30 12:57:32 -0500

    221. Información del estado de las peticiones

--- 09-heroesApp/src/app/pages/heroe/heroe.component.ts
+++ 09-heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,37 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { HeroeModel } from '../../models/heroe.model';
-import { NgForm } from '@angular/forms';
-import { HeroesService } from '../../service/heroes.service';
-import swal from 'sweetalert2';
+import { Component, OnInit } from "@angular/core";
+import { HeroeModel } from "../../models/heroe.model";
+import { NgForm } from "@angular/forms";
+import { HeroesService } from "../../service/heroes.service";
+import swal from "sweetalert2";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-heroe',
-  templateUrl: './heroe.component.html',
-  styleUrls: ['./heroe.component.css']
+  selector: "app-heroe",
+  templateUrl: "./heroe.component.html",
+  styleUrls: ["./heroe.component.css"],
 })
 export class HeroeComponent implements OnInit {
   heroe = new HeroeModel();
-  titularAlerta:string;
+  titularAlerta: string;
 
-  constructor(private heroeService: HeroesService) { }
+  constructor(private heroeService: HeroesService) {}
 
-  ngOnInit(): void {
-  }
-  guardar(form: NgForm){
-    if(form.invalid){
-      swal.fire('Formulario no valido', this.titularAlerta, 'error');
+  ngOnInit(): void {}
+  guardar(form: NgForm) {
+    if (form.invalid) {
+      swal.fire("Formulario no valido", this.titularAlerta, "error");
       return;
     }
-    if(this.heroe.id ){
-      this.heroeService.actualizarHeroe(this.heroe).subscribe( () => {
-        swal.fire('Actualizado Correctamente', this.titularAlerta, 'success');
-      });
-    }else{
-      this.heroeService.crearHeroe(this.heroe).subscribe(resp => {
-        swal.fire('Enviado Correctamente', this.titularAlerta, 'success');
-        this.heroe = resp;
-      });
-    }
+    swal.fire({
+      title: "Espere",
+      text: "Guardando informacion",
+      icon: "info",
+      allowOutsideClick: false,
+    });
+    swal.showLoading();
+    let peticion: Observable<any>;
 
+    if (this.heroe.id) {
+      peticion = this.heroeService.actualizarHeroe(this.heroe);
+      // swal.fire('Actualizado Correctamente', this.titularAlerta, 'success');
+    } else {
+      peticion = this.heroeService.crearHeroe(this.heroe);
+      //  swal.fire('Enviado Correctamente', this.titularAlerta, 'success');
+    }
+    peticion.subscribe(resp => {
+          swal.fire({
+            title: this.heroe.nombre,
+            text: "Se actualizo correctamente",
+            icon: "success"
+          });
+    });
   }
 }
