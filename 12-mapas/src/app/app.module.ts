d48b36e3082f2d7f052862579f1495ba7849ab31
jguerrero101314 <joelg1014@hotmail.com>
2020-06-01 11:44:37 -0500

    259. Utilizar componentes de Angular Material

--- 12-mapas/src/app/app.module.ts
+++ 12-mapas/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+//angular material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
