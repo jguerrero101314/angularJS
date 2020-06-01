3ec676fe72ea8b2471faa6f430688d9ac96a279c
jguerrero101314 <joelg1014@hotmail.com>
2020-06-01 14:05:41 -0500

    262. Mostrando un mapa de Google

--- 12-mapas/src/app/app.module.ts
+++ 12-mapas/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AgmCoreModule } from '@agm/core';
+import { GoogleMapsModule } from "@angular/google-maps";
+import { MaterialModule } from './material.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
+
 import { MapaComponent } from './components/mapa/mapa.component';
 
 
@@ -16,7 +20,12 @@ import { MapaComponent } from './components/mapa/mapa.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    GoogleMapsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBG9HMhkqAHBIYFQrIGB1blKXSyzgPmz5Q'
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
