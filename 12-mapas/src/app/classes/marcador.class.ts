835331ea3dbf14efb70cee2b614ac1a7c2b67ff4
jguerrero101314 <joelg1014@hotmail.com>
2020-06-01 14:23:03 -0500

    263. Creando marcadores de forma dinámica

---
+++ 12-mapas/src/app/classes/marcador.class.ts
@@ -0,0 +1,15 @@
+// export class Marcador {
+//   constructor(public lat: number, public lng: number){
+//   }
+// }
+
+export class Marcador {
+  public lat: number;
+  public lng: number;
+  public titulo: string = 'Sin Titulo';
+  public desc: string = 'Sin descripcion';
+  constructor( lat: number,  lng: number){
+    this.lat = lat;
+    this.lng = lng;
+  }
+}
--- 12-mapas/src/app/components/mapa/mapa.component.ts
+++ 12-mapas/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marcador } from '../../classes/marcador.class';
 
 @Component({
   selector: 'app-mapa',
@@ -6,12 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
+  marcadores: Marcador[] = [];
 
   lat = 51.678418;
   lng = 7.809007;
-  constructor() { }
+  constructor() {
+    const marcadorNuevo = new Marcador(51.678418,7.809007);
+    this.marcadores.push(marcadorNuevo);
+  }
 
   ngOnInit(): void {
+  }
+  agregarMarcador(evento){
+    const coords: { lat: number, lng: number} = evento.coords;
+    console.log(evento.coords.lat);
+    const marcadorNuevo = new Marcador(coords.lat, coords.lng);
+    this.marcadores.push(marcadorNuevo);
   }
 
 }
