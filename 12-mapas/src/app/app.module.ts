cde5dc3248165192504481f1347e8a26cf066ff4
jguerrero101314 <joelg1014@hotmail.com>
2020-06-01 15:20:48 -0500

    267. Dialog - Mostrar un modal para la edición del marcador

--- 12-mapas/src/app/app.module.ts
+++ 12-mapas/src/app/app.module.ts
@@ -9,13 +9,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MapaComponent } from './components/mapa/mapa.component';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
 
 
 
 @NgModule({
+  entryComponents:[
+      MapaEditarComponent
+  ],
   declarations: [
     AppComponent,
-    MapaComponent
+    MapaComponent,
+    MapaEditarComponent
   ],
   imports: [
     BrowserModule,
