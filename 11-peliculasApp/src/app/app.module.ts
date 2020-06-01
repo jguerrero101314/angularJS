383a9b0ec60523fe05078cb238bbd93b9281090f
jguerrero101314 <joelg1014@hotmail.com>
2020-06-01 07:59:00 -0500

    246. Tarea de película

--- 11-peliculasApp/src/app/app.module.ts
+++ 11-peliculasApp/src/app/app.module.ts
@@ -5,10 +5,16 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
