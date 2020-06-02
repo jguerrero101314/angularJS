cab3424c68eecc836dc017a68e68449b955adada
jguerrero101314 <joelg1014@hotmail.com>
2020-06-02 07:40:22 -0500

    279. Mostrando los videos en tarjetas

--- 13-youtubeApp/src/app/pages/home/home.component.ts
+++ 13-youtubeApp/src/app/pages/home/home.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from "@angular/core";
 import { YoutubeService } from "../../service/youtube.service";
+import { Video } from '../../models/youtube.models';
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styles: [],
+  styles: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  videos: Video[] = [];
   constructor(private youtubeService: YoutubeService) {}
 
   ngOnInit(): void {
     this.youtubeService.getVideos().subscribe((resp) => {
-      console.log(resp);
+      this.videos.push(...resp);
+      console.log(this.videos);
     });
   }
 }
