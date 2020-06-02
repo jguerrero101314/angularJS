65f82741a0c644ed810f9a147c4e23e40ae1892f
jguerrero101314 <joelg1014@hotmail.com>
2020-06-02 07:48:45 -0500

    280. Mostrar un modal con el video

--- 13-youtubeApp/src/app/pages/home/home.component.ts
+++ 13-youtubeApp/src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { YoutubeService } from "../../service/youtube.service";
 import { Video } from '../../models/youtube.models';
+import Swal from 'sweetalert2';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: "app-home",
@@ -16,5 +18,22 @@ export class HomeComponent implements OnInit {
       this.videos.push(...resp);
       console.log(this.videos);
     });
+  }
+  mostrarVideo( video: Video){
+    console.log(video);
+    Swal.fire({
+      html: `
+      <h4>${video.title}</h4>
+      <hr>
+      <iframe width="100%"
+              height="315"
+              src="https://www.youtube.com/embed/${video.resourceId.videoId}"
+              frameborder="0"
+              allow="accelerometer;
+              autoplay; encrypted-media;
+              gyroscope; picture-in-picture"
+              allowfullscreen></iframe>
+      `
+    })
   }
 }
