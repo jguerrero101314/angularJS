8143ff141990adf260e4119dd578720e3c1f4de4
jguerrero101314 <joelg1014@hotmail.com>
2020-06-03 09:06:03 -0500

    331. Drag básico

---
+++ virtualScrool/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { ScrollingModule  } from '@angular/cdk/scrolling';
+import { DragDropModule  } from '@angular/cdk/drag-drop';
+
+import { AppComponent } from './app.component';
+import { VirtualComponent } from './virtual/virtual.component';
+import { DragComponent } from './drag/drag.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    VirtualComponent,
+    DragComponent
+  ],
+  imports: [
+    BrowserModule,
+    ScrollingModule,
+    DragDropModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
