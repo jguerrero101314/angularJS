5b7efd6e3f23e3e762b3535e84221d4ee6823578
jguerrero101314 <joelg1014@hotmail.com>
2020-06-03 09:28:48 -0500

    333. Drag & Drop de una lista ordenada

--- 17-virtualScrool/src/app/app.module.ts
+++ 17-virtualScrool/src/app/app.module.ts
@@ -4,21 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { ScrollingModule  } from '@angular/cdk/scrolling';
 import { DragDropModule  } from '@angular/cdk/drag-drop';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { VirtualComponent } from './virtual/virtual.component';
 import { DragComponent } from './drag/drag.component';
+import { PaisesComponent } from './paises/paises.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VirtualComponent,
-    DragComponent
+    DragComponent,
+    PaisesComponent
   ],
   imports: [
     BrowserModule,
     ScrollingModule,
-    DragDropModule
+    DragDropModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
