3947465703b526c839167902cabbb490d23e9488
Joel Guerrero <joelg1014@hotmail.com>
2020-06-04 09:07:16 -0500

    305. Validaciones del archivo desde la directiva

--- 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 
 @Directive({
@@ -6,18 +7,48 @@ import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from
 })
 export class NgDropFilesDirective {
 
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
 
-  @HostListener('dragover',['$event'])
-  public onDragEvent(event: any){
-    this.mouseSobre.emit(true);
+  @HostListener('dragover', ['$event'])
+  public onDragEnter( event: any ) {
+    this.mouseSobre.emit( true );
   }
-  @HostListener('dragleave',['$event'])
-  public onDragLeave(event: any){
-    this.mouseSobre.emit(false);
 
+  @HostListener('dragleave', ['$event'])
+  public onDragLeave( event: any ) {
+    this.mouseSobre.emit( false );
+  }
+
+  // validaciones
+  private _archivoPuedeSerCargado(archivo:File): boolean{
+    if(!this._archivosYaFueDroppeado(archivo.name) && this._esImagen(archivo.type)){
+        return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+
+  private _prevenirDetener( event){
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivosYaFueDroppeado( nombreArhivo: string): boolean{
+    for(const archivo of this.archivos){
+      if(archivo.nombreArchivo === nombreArhivo){
+        console.log('el archivo', + nombreArhivo + ' ya esta agregado');
+        return true;
+      }
+    }
+    return false;
+  }
+  private _esImagen( tipoArchivo: string): boolean{
+    return (tipoArchivo === '' || tipoArchivo === undefined) ? false : tipoArchivo.startsWith('image');
   }
 
 }
