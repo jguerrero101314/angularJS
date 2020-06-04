a3bc20c22a759613a0ee6a482fb0e26d2a968a4f
Joel Guerrero <joelg1014@hotmail.com>
2020-06-04 09:24:17 -0500

    306. Obtener archivos y validarlos

--- 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
+++ 15-fotosFirebase/src/app/directives/ng-drop-files.directive.ts
@@ -1,20 +1,25 @@
-import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import {
+  Directive,
+  EventEmitter,
+  ElementRef,
+  HostListener,
+  Input,
+  Output,
+} from "@angular/core";
 import { FileItem } from '../models/file-item';
 
-
 @Directive({
-  selector: '[appNgDropFiles]'
+  selector: "[appNgDropFiles]",
 })
 export class NgDropFilesDirective {
-
   @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
-  constructor() { }
-
+  constructor() {}
   @HostListener('dragover', ['$event'])
   public onDragEnter( event: any ) {
     this.mouseSobre.emit( true );
+    this._prevenirDetener( event );
   }
 
   @HostListener('dragleave', ['$event'])
@@ -22,33 +27,78 @@ export class NgDropFilesDirective {
     this.mouseSobre.emit( false );
   }
 
-  // validaciones
-  private _archivoPuedeSerCargado(archivo:File): boolean{
-    if(!this._archivosYaFueDroppeado(archivo.name) && this._esImagen(archivo.type)){
-        return true;
+  @HostListener('drop', ['$event'])
+  public onDrop( event: any ) {
+
+    const transferencia = this._getTransferencia( event );
+
+    if ( !transferencia ) {
+      return;
     }
-    else{
-      return false;
+
+    this._extraerArchivos( transferencia.files );
+
+    this._prevenirDetener( event );
+    this.mouseSobre.emit( false );
+
+  }
+
+  private _getTransferencia( event: any ) {
+    return event.dataTransfer ? event.dataTransfer : event.originalEvent.dataTransfer;
+  }
+
+  private _extraerArchivos( archivosLista: FileList ) {
+
+    // console.log( archivosLista );
+
+    // tslint:disable-next-line:forin
+    for ( const propiedad in Object.getOwnPropertyNames( archivosLista ) ) {
+
+      const archivoTemporal = archivosLista[propiedad];
+
+      if ( this._archivoPuedeSerCargado( archivoTemporal ) ) {
+
+        const nuevoArchivo = new FileItem( archivoTemporal );
+        this.archivos.push( nuevoArchivo );
+
+      }
     }
+    console.log(this.archivos);
   }
 
 
-  private _prevenirDetener( event){
+  // Validaciones
+  private _archivoPuedeSerCargado( archivo: File ): boolean {
+
+    if ( !this._archivoYaFueDroppeado( archivo.name ) && this._esImagen( archivo.type ) ){
+      return true;
+    }else {
+      return false;
+    }
+
+  }
+
+
+  private _prevenirDetener( event ) {
     event.preventDefault();
     event.stopPropagation();
   }
 
-  private _archivosYaFueDroppeado( nombreArhivo: string): boolean{
-    for(const archivo of this.archivos){
-      if(archivo.nombreArchivo === nombreArhivo){
-        console.log('el archivo', + nombreArhivo + ' ya esta agregado');
+  private _archivoYaFueDroppeado( nombreArchivo: string ): boolean {
+
+    for ( const archivo of this.archivos ) {
+
+      if ( archivo.nombreArchivo === nombreArchivo  ) {
+        console.log('El archivo ' + nombreArchivo + ' ya esta agregado');
         return true;
       }
+
     }
+
     return false;
   }
-  private _esImagen( tipoArchivo: string): boolean{
-    return (tipoArchivo === '' || tipoArchivo === undefined) ? false : tipoArchivo.startsWith('image');
-  }
 
+  private _esImagen( tipoArchivo: string ): boolean {
+    return ( tipoArchivo === '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
+  }
 }
