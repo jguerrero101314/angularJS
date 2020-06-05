8eb84d21cad582dfd2cc1b76fd33eee7ee94a538
Joel Guerrero <joelg1014@hotmail.com>
2020-06-05 09:56:55 -0500

    347. Servidor Express dentro de Firebase Cloud Functions

--- 18-gotyBackend/functions/src/index.ts
+++ 18-gotyBackend/functions/src/index.ts
@@ -1,22 +1,36 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import * as express from "express";
+import * as cors from "cors";
 const serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gameoftheyear-226d3.firebaseio.com",
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://gameoftheyear-226d3.firebaseio.com",
+});
+const db = admin.firestore();
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.json({
+        mensaje: "Hola Mundo desde Funciones de Firebase!!!",
+    });
 });
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-  response.json({
-    mensaje: "Hola mundo desde funciones de firebase!!!",
-  });
+export const getGOTY = functions.https.onRequest(async (request, response) => {
+    // const nombre = request.query.nombre || 'Sin Nombre';
+    const gotyRef = db.collection("goty");
+    const docsSnap = await gotyRef.get();
+    const juegos = docsSnap.docs.map((doc) => doc.data());
+
+    response.json(juegos);
 });
-export const getGoty = functions.https.onRequest((request, response) => {
-    const nombre = ' Joel ';
-    response.json({
-        nombre
-    })
-  });
+
+// Express
+const app = express();
+app.use(cors({ origin: true }));
+
+app.get("/goty", async (req, res) => {
+    const gotyRef = db.collection("goty");
+    const docsSnap = await gotyRef.get();
+    const juegos = docsSnap.docs.map((doc) => doc.data());
+
+    res.json(juegos);
+});
