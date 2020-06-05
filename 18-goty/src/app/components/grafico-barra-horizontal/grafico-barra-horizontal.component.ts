5d180611e728611e357e2d00f665df3be3172ba5
Joel Guerrero <joelg1014@hotmail.com>
2020-06-05 08:15:17 -0500

    342. Cambiar valores de la gráfica de forma aleatoria

--- 18-goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ 18-goty/src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,29 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from "@angular/core";
 
 @Component({
-  selector: 'app-grafico-barra-horizontal',
-  templateUrl: './grafico-barra-horizontal.component.html',
-  styleUrls: ['./grafico-barra-horizontal.component.css']
+  selector: "app-grafico-barra-horizontal",
+  templateUrl: "./grafico-barra-horizontal.component.html",
+  styleUrls: ["./grafico-barra-horizontal.component.css"],
 })
-export class GraficoBarraHorizontalComponent {
-
-  results: any[] =[
+export class GraficoBarraHorizontalComponent implements OnDestroy {
+  results: any[] = [
     {
-      "name": "Juego 1",
-      "value": 20
+      // tslint:disable-next-line: quotemark
+      name: "Juego 1",
+      value: 20,
     },
     {
-      "name": "Juego 2",
-      "value": 25
+      name: "Juego 2",
+      value: 25,
     },
     {
-      "name": "Juego 3",
-      "value": 15
+      name: "Juego 3",
+      value: 15,
     },
     {
-      "name": "Juego 4",
-      "value": 40
-    }
+      name: "Juego 4",
+      value: 40,
+    },
   ];
 
   // options
@@ -32,16 +32,30 @@ export class GraficoBarraHorizontalComponent {
   gradient = true;
   showLegend = true;
   showXAxisLabel = true;
-  xAxisLabel = 'Juegos';
+  xAxisLabel = "Juegos";
   showYAxisLabel = true;
-  yAxisLabel = 'Votos';
+  yAxisLabel = "Votos";
 
-  colorScheme = 'nightLights'
+  colorScheme = "nightLights";
+  intervalo;
 
-  constructor() { }
+  constructor() {
+    console.log('tick');
+    const newResults = [...this.results];
+    this.intervalo = setInterval(() => {
+      // tslint:disable-next-line: prefer-const
+      // tslint:disable-next-line: forin
+      for (let i in newResults) {
+        newResults[i].value = Math.round(Math.random() * 500);
+      }
+      this.results = [...newResults];
+    }, 1500);
+  }
 
   onSelect(event) {
     console.log(event);
   }
-
+  ngOnDestroy(){
+    clearInterval(this.intervalo);
+  }
 }
